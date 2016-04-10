2cc90cf794f95fb13b959e3e87e03294197964a8
Joshua Tacoma <joshua.tacoma@gmail.com>
2016-04-10 02:12:13 -0400

    A yellow Sun in a blue sky

--- pebble/src/analog_layer.c
+++ pebble/src/analog_layer.c
@@ -47,7 +47,34 @@ typedef struct {
     struct tm wall_time;
 } JTT_AnalogData;
 
+static GRect jtt_rect_trim (const GRect rect, const int8_t trim) {
+    const GRect result = {
+        .origin = {
+            .x = rect.origin.x + trim,
+            .y = rect.origin.y + trim,
+        },
+        .size = {
+            .w = rect.size.w - trim*2,
+            .h = rect.size.h - trim*2,
+        },
+    };
+    return result;
+}
+
 static void jtt_analog_layer_update (Layer *layer, GContext *ctx) {
+    const GColor color_sun_fill = GColorYellow;
+    const GColor color_sun_stroke = GColorOrange;
+    const GColor color_sky_fill [] = {
+        //GColorPictonBlue,
+        //GColorVividCerulean,
+        GColorCyan,
+        GColorElectricBlue,
+        GColorCeleste,
+    };
+    const int8_t color_sky_fill_len =
+        sizeof(color_sky_fill) / sizeof(color_sky_fill[0]);
+    const GColor color_sky_stroke = GColorBlueMoon;
+
     const JTT_AnalogData * const data = layer_get_data(layer);
     const GRect bounds = layer_get_bounds(layer);
 
@@ -56,23 +83,65 @@ static void jtt_analog_layer_update (Layer *layer, GContext *ctx) {
 
     // TODO: find value of inset_thickness based on available size, or
     //       set through public API of this module.
-    const int16_t inset_thickness =
-        (bounds.size.w > bounds.size.h)
-        ? bounds.size.h / 8
-        : bounds.size.w / 8;
-    graphics_context_set_fill_color(ctx, GColorBlack);
-    const int32_t angle_start = TRIG_MAX_ANGLE / 2;
-    const int32_t angle_delta =
-        TRIG_MAX_ANGLE * data->wall_time.tm_hour / 24
-        +
-        TRIG_MAX_ANGLE * data->wall_time.tm_min / (24 * 60);
-    graphics_fill_radial(
-            ctx,
-            bounds,
+    const GRect sky_bounds = bounds;
+    const int16_t sky_thickness =
+        (sky_bounds.size.w > sky_bounds.size.h)
+        ? sky_bounds.size.h / 6
+        : sky_bounds.size.w / 6;
+    for (int8_t sky_fill=0; sky_fill<color_sky_fill_len; ++sky_fill) {
+        graphics_context_set_fill_color(ctx, color_sky_fill[sky_fill]);
+        graphics_fill_radial(
+                ctx,
+                jtt_rect_trim(
+                    sky_bounds,
+                    sky_thickness * sky_fill / color_sky_fill_len),
+                GOvalScaleModeFitCircle,
+                sky_thickness / color_sky_fill_len,
+                0,
+                TRIG_MAX_ANGLE);
+    }
+    const int32_t midnight_angle = TRIG_MAX_ANGLE / 2;
+    const GRect sky_inset = jtt_rect_trim(sky_bounds, sky_thickness);
+    graphics_context_set_stroke_color(ctx, color_sky_stroke);
+    graphics_context_set_antialiased(ctx, true);
+    for (int32_t hour = 0; hour < 24; ++hour) {
+        const int32_t hour_angle =
+            (midnight_angle + hour * TRIG_MAX_ANGLE / 24)
+            % TRIG_MAX_ANGLE;
+        const GPoint p0 = gpoint_from_polar(
+                sky_inset,
+                GOvalScaleModeFitCircle,
+                hour_angle);
+        const GPoint p1 = gpoint_from_polar(
+                bounds,
+                GOvalScaleModeFitCircle,
+                hour_angle);
+        graphics_context_set_stroke_width(ctx, hour % 3 ? 1 : 3);
+        graphics_draw_line(ctx, p0, p1);
+    }
+    const int32_t sun_angle = midnight_angle
+        + TRIG_MAX_ANGLE * data->wall_time.tm_hour / 24
+        + TRIG_MAX_ANGLE * data->wall_time.tm_min / (24 * 60);
+    const int32_t sun_diameter = sky_thickness*2/3;
+    const GRect sun_orbit = jtt_rect_trim(sky_bounds, sun_diameter/2);
+    const GPoint sun_center = gpoint_from_polar(
+            sun_orbit,
             GOvalScaleModeFitCircle,
-            inset_thickness,
-            angle_start,
-            angle_start + angle_delta);
+            sun_angle);
+    graphics_context_set_stroke_color(ctx, color_sun_stroke);
+    graphics_context_set_stroke_width(ctx, 2);
+    graphics_draw_line(
+            ctx,
+            sun_center,
+            gpoint_from_polar(
+                sky_inset,
+                GOvalScaleModeFitCircle,
+                sun_angle));
+    graphics_context_set_fill_color(ctx, color_sun_fill);
+    graphics_fill_circle(ctx, sun_center, sun_diameter/2);
+    graphics_context_set_stroke_color(ctx, color_sun_stroke);
+    graphics_context_set_stroke_width(ctx, 2);
+    graphics_draw_circle(ctx, sun_center, sun_diameter/2);
 }
 
 struct JTT_AnalogLayer {
@@ -114,4 +183,5 @@ void jtt_analog_layer_set_time(
     analog_layer->data->unix_time = time;
     struct tm * wall_time = localtime(&time);
     analog_layer->data->wall_time = *wall_time;
+    layer_mark_dirty(analog_layer->layer);
 }
