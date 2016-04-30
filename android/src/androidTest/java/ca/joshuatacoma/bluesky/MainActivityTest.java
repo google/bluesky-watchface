eb8deff156ebaa85a9343e22880cf595b6b39fcd
Joshua Tacoma <joshua.tacoma@gmail.com>
2016-04-30 16:17:55 -0400

    Initial import

---
+++ android/src/androidTest/java/ca/joshuatacoma/bluesky/MainActivityTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.joshuatacoma.bluesky;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class ca.joshuatacoma.bluesky.MainActivityTest \
+ * ca.joshuatacoma.bluesky.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("ca.joshuatacoma.bluesky", MainActivity.class);
+    }
+
+}
