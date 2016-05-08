5e52c2681430867f1f2ac9e18a8ccfe544f2e55b
Joshua Tacoma <joshua.tacoma@gmail.com>
2016-05-07 20:29:11 -0400

    Don't always request fresh data

--- pebble/src/modules/data.c
+++ pebble/src/modules/data.c
@@ -339,9 +339,7 @@ void bsky_data_update(void) {
     {
         APP_LOG(APP_LOG_LEVEL_DEBUG,
                 "bsky_data_update: nothing to do");
-        //return; TODO: stop sending extra messages
-        APP_LOG(APP_LOG_LEVEL_WARNING,
-                "bsky_data_update: doing it anyway just to test");
+        return;
     }
 
     // Send update using app_sync_set
